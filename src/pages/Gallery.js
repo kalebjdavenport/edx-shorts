4350b0d82645ad601ef1305703dff882a8cffe81
Jesper Hodge <jhodge@outlook.de>
2023-10-24 12:09:56 -0400

    feat: add pages

---
+++ src/pages/Gallery.js
@@ -0,0 +1,7 @@
+export default function Gallery() {
+  return (
+    <h1 className="text-3xl font-bold underline mt-8 text-center">
+      Gallery
+    </h1>
+  );
+}
---
+++ src/pages/Player.js
@@ -0,0 +1,7 @@
+export default function Player() {
+  return (
+    <h1 className="text-3xl font-bold underline mt-8 text-center">
+      Player
+    </h1>
+  );
+}
